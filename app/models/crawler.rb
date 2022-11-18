487dc1a43bec160e368d52066a3676f13e299472
“abhiyankhanal” <abhiyan5588>
2022-11-18 15:43:48 +0545

    controllers

---
+++ app/models/crawler.rb
@@ -0,0 +1,6 @@
+class Crawler < ApplicationRecord
+    # validates :url, presence: true, length: {minimum: 10}
+    # validates :title, presence: true, length: { minimum: 10 }
+    # validates :price, presence: true, length: { minimum: 10 }
+    # validates :stars, presence: true, length: { minimum: 10 }
+end
